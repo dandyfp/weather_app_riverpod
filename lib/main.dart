33e8f9aba2f200c9f731d40391fb86afa6af9854
dandyfp <dandyfp@sakutambah.id>
2023-11-15 14:51:40 +0700

    Clear comment

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,6 @@ class MyApp extends ConsumerWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
---
+++ test/data/repository/weather_repository_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_app/data/repository/weather_repository.dart';
+
+class MockWeatherRepository extends Mock implements WeatherRepository {}
+
+void main() {
+  group('WeatherRepository Tests', () {
+    testWidgets('weather repository test', (tester) async {});
+  });
+}
